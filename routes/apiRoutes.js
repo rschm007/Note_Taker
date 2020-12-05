acb935896d4a474d12c5bd17dc44b2356c70219f
Robert Schmahl <rschm007@gmail.com>
2020-12-04 16:26:36 -0800

    trying new methods for apiRoutes. Now using fs to read & write files. Using JSON methods to read data, parse, stringify and send to client

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,41 +1,100 @@
 // ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// DEPENDENCIES
 // ===============================================================================
-
-const noteData = require("../db/db");
+const fs = require("fs");
+const path = require("path");
 
 // ===============================================================================
-// ROUTING
+// API ROUTES
 // ===============================================================================
 
-module.exports = function(app) {
-  // API GET
-  // ---------------------------------------------------------------------------
-
-    // read api/notes and return data as JSON
-  app.get("/api/notes", function(req, res) {
+module.exports = function (app) {
+  // GET notes
+  app.get("/api/notes",  (err, res) => {
+    try {
+      // read notes from JSON
+      noteData = fs.readFileSync("./db/db.json", "utf8");
+      // parse JSON object into an array of objects
+      noteData = JSON.parse(noteData);
+    }
+    // catch err
+    catch (err) {
+      console.log(err);
+    }
+    // send array of objects to client
     res.json(noteData);
   });
 
-  // API POST
-    // ---------------------------------------------------------------------------
-
-    // receive a new note, save on request body, 
-    // add to db.json, and return new note to client
-  app.post("/api/notes", function(req, res) {
+  // POST notes
+  app.post("/api/notes",  (req, res) => {
+    try {
+      // read notes from JSON
+      noteData = fs.readFileSync("./db/db.json", "utf8");
+      
+      // parse JSON object into an array of objects
+      noteData = JSON.parse(noteData);
+      // assign a new unique id
+      req.body.id = noteData.length;
+      // push new noteData to the object array (req.body is the user input)
       noteData.push(req.body);
-      res.json(noteData);
-    });
+      // stringify it via JSON so it can be written to file
+      noteData = JSON.stringify(noteData);
+      // write new note to the file
+      fs.writeFile("./db/db.json", noteData, "utf8", (err) => {
+        // err handling
+        if (err) throw err;
+      });
+      // convert back to an array of objects and send to client
+      res.json(JSON.parse(noteData));
+    }
+    // catch err
+    catch (err) {
+      console.log(err);
+    }
+  });
 
-  // API DELETE
-    // ---------------------------------------------------------------------------
-    // receive a query param containing id of a note to delete
-    // read all notes from db.json and remove the relevant note w/ appropriate ID
-    // rewrite notes to db.json files
-    app.delete("/api/notes", function(req, res) {
-        noteData.
-        res.json(noteData);
-    });
+  // RETRIEVE notes by calling specific ID
+  app.get("/api/notes/:id", function (req, res) {
+    // display JSON object of speficic note array
+    // we find the specific note with req.params.id, which returns the ID parameter.
+    noteData.json(noteData[req.param.id]);
+  });
+
+  // DELETE notes
+  // use a specific id to identify the note we want to delete
+  app.delete("/api/notes/:id", (req, res) => {
+    try {
+      // read notes from JSON
+      noteData = fs.readFileSync("./db/db.json", "utf8");
+      // parse JSON object into an array of objects
+      noteData = JSON.parse(noteData);
+      // delete the old note using filter method
+      noteData = noteData.filter((note) => {
+        // return if the note id is not the same as requested id
+        return note.id != req.params.id;
+      });
+      // stringify it via JSON so it can be written to file
+      noteData = JSON.stringify(noteData);
+      // write new note to the file
+      fs.writeFile("./db/db.json", noteData, "utf8", (err) => {
+        // err handling
+        if (err) throw err;
+      });
+      // convert back to an array of objects and send to client
+      res.json(JSON.parse(noteData));
+      }
+      // catch err
+      catch (err) {
+      console.log(err);
+      }
+  });
+
+  // DEFINE updateDB function
+  function updateDB() {
+    fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
+      if (err) throw err;
+      return true
+    })
+  }
+
 };
--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 // Dependencies
 // =============================================================
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
 // ==============================================================================
-
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-const PORT = process.env.PORT || 3000;
-
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// render static files
 app.use(express.static('public'));
 
 
